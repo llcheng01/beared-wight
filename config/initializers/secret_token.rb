6b3ac4df65c343de744bbfe05d52739828051866
Johnny Cheng <jcheng@tribune.com>
2014-08-19 11:02:58 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BearedWight::Application.config.secret_token = '6b870addb923e3c6bfe33905e6c5a741bbdbfcfc7229d2bdfa73a80067f681c6b1de568c0db6219675aa9b08cef541d25f828f520d7426254efbf745784f9446'
